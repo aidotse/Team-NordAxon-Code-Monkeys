f5b6b4af0351766d6a65eb36ff19634aaf213191
Jairo Alfonso <llanojairo@users.noreply.github.com>
2020-10-31 16:23:20 +0100

    Improved class to apply mask to raw images per magnification. Output of final masks still needs some work

--- AZHackathon/src/features/maskslic.py
+++ AZHackathon/src/features/maskslic.py
@@ -1,76 +1,55 @@
 import glob
-from typing import List
+from typing import List, Dict
 
-from joblib import Parallel, delayed
-# from natsort import natsorted, ns
 import numpy as np
 from skimage import io
 from skimage import morphology
 
 
-class MaskSlic:
+class Mask:
     """
-    Apply MaskSlic
-    :param path: Any valid string path or os.PathLike object.
+    Apply Mask to each magnification's target images
+    Expected folders structure in input_path:
+    input_path
+        - magnification1
+            - inputs
+            - target
+        - magnification2
+            - inputs
+            - target
+        ...
+    :param input_path: valid string path or os.PathLike object that contains
+                       target images per magnification (details above)
+    :param output_path: valid string path or os.PathLike object to store
+                        output masks for each magnification found in input_path
     """
-    def __init__(self, path=None, output_path=None):
-        self.path = path
+    def __init__(self, input_path=None, output_path=None):
+        self.input_path = input_path
         self.output_path = output_path
+        self.images = None
+        self.target_paths = None
+        self.target_masks = None
 
-    def apply_maskslic(self):
+    def store_result(self):
         """
-        Apply MaskSlic to file paths in list returned by get_files_paths()
+        TODO
         """
-        images_paths = self.get_files_paths()
+        self.apply_mask()
+        # add here code to write dict values (final masks) to output_path
 
-        for img_path in images_paths:
-            img_obj = io.imread(img_path)
-            mask = self.mask_slic(img_obj)
-            io.imsave('{}/{}_mask.tif'.format(self.output_path, img_path.name), mask)
-
-    def apply_maskslic_parallel(self):
+    def apply_mask(self):
         """
-        Apply MaskSlic to images in paralell
+        Apply Mask to target image objects per magnification
         """
-        images_np_lst = self.read_images()
-        Parallel(n_jobs=4)(delayed(self.mask_slic)(i) for i in images_np_lst)
+        self.read_images()
+        target_masks = {k: [mask(v) for v in self.images[k]]
+                        for k, v in self.images}
 
-    # def apply_maskslic_V2(self):
-    #     """
-    #     Apply MaskSlic to files found in raw data folder
-    #     """
-    #     filename = ...
-    #     images_np_lst = self.read_images()
-    #     for image_obj in images_np_lst:
-    #         mask = self.mask_slic(image_obj)
-    #         io.imsave('{}/{}.tif'.format(self.output_path, filename), mask)
+        self.target_masks = target_masks
 
-    def get_files_paths(self) -> List[str]:
+    def mask(self, img) -> np.ndarray:
         """
-        Create list with files/images paths found in raw data directory/input path
-
-        Returns:
-        list: list with paths to each raw image found in raw data dir
-        """
-        files_paths = [f for f in glob.glob(self.path + "/*.tif", recursive=False)]
-
-        return files_paths
-
-    def read_images(self) -> List[np.ndarray]:
-        """
-        Create list with images/numpy objects from paths returned by get_file_paths()
-
-        Returns:
-        list: list with paths to each raw image found in raw data dir
-        """
-        raw_imgs_paths = self.get_file_paths()
-        img_objs_list = [io.imread(file_path) for file_path in raw_imgs_paths]
-
-        return img_objs_list
-
-    def mask_slic(self, img) -> np.ndarray:
-        """
-        Scikit-image's MaskSlic
+        Scikit-image's Mask
 
         Returns:
         mask: scikit-image object (ndarray)
@@ -82,3 +61,35 @@ class MaskSlic:
         mask = morphology.opening(mask, morphology.disk(3))
 
         return mask
+
+    def get_target_files_paths(self) -> Dict[str, List[str]]:
+        """
+        Create dictionary with magnification names as keys (e.g., 20x, 40x).
+        Each key contains a list with paths to target images/files
+        for a magnification.
+
+        Returns:
+        magn_target_paths: dict with magnification:[target_paths] as key:value
+        """
+        target_folders = [i for i in glob.glob(os.path.join(self.input_path, "/*/*/")
+                                               if "targets" in Path(i).name.lower()]
+        magn_target_paths = {}
+        for folder_path in target_folders:
+            magn_target_paths[Path(folder_path).parent.name] = glob.glob(folder_path + '/*.tif')
+
+        self.target_paths = magn_target_paths
+
+    def read_images(self) -> Dict[str, List[np.ndarray]]:
+        """
+        Create dictionary with magnification names as keys (e.g., 20x, 40x).
+        Each key contains a list with a magnification's targets
+        as image objetcs (scikit-image IO)
+
+        Returns:
+        magn_img_objs: dict with magnification:[target_image_objs] as key:value
+        """
+        self.get_target_files_paths()
+        magn_img_objs = {k: [io.imread(v) for v in get_target_files_paths()[k]]
+                         for k, v in self.get_target_files_paths().items()}
+
+        self.images = magn_img_objs
