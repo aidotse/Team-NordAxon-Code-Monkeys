3746d0b182ffe8f46ed77895de242a5c59fcf837
EliteDarkLordDragonSlayer3.14159 <david.bao.fu@gmail.com>
2020-11-15 15:26:19 +0100

    Updated predict.py

--- AZHackathon/src/predict.py
+++ AZHackathon/src/predict.py
@@ -132,12 +132,12 @@ def test_time_augmentation_predict(inputs:torch.Tensor, model:nn.Module, device:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='pix2pix-pytorch-implementation')
-    parser.add_argument('--input_dir', type=str, default='../data/03_training_data/normalized_bias/train/input/20x_images', help='a2b or b2a')
-    parser.add_argument('--output_dir', type=str, default="../data/04_generated_images/20x_images", help='input image channels')
-    parser.add_argument('--weights_path', type=str, default="../../data/05_saved_models/A2_g_best.pth", help='output image channels')
+    parser.add_argument('--input-dir', type=str, default='../data/03_training_data/normalized_bias/train/input/20x_images', help='a2b or b2a')
+    parser.add_argument('--output-dir', type=str, default="../data/04_generated_images/20x_images", help='input image channels')
+    parser.add_argument('--weights-path', type=str, default="../../data/05_saved_models/A2_g_best.pth", help='output image channels')
     parser.add_argument('--target', type=str, default="A2", help="'A1', 'A2', 'A3' or 'all'")
     parser.add_argument('--mask', action='store_true')
-    parser.add_argument('--match_histogram', type=bool, default=False, help="Match histograms with training data as post-processing.")
+    parser.add_argument('--match-histogram', type=bool, default=False, help="Match histograms with training data as post-processing.")
     parser.add_argument('--mag', type=bool, default=False, help="Either 20x, 40x or 60x (Used when matching histograms).")
 
     opt = parser.parse_args()
